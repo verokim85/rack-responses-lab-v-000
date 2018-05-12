54536ae1c76503e4d20f45b6d4da73b05f37b7a5
Veronica Kim <vero.a.kim@gmail.com>
2018-05-12 15:14:18 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,11 +3,14 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-
+    if
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
 
 
 
     resp.finish
   end
-  
+
 end

cf6ec9886ec37ddfb2d6188afacbed3a719e0e20
Veronica Kim <vero.a.kim@gmail.com>
2018-05-12 15:17:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,11 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    if time.now = x < 12PM 
+    if time.now = x < 12PM
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
-
+    end
 
 
     resp.finish
